354f53dc68587bfad9380d843e3d3e27e255dcb3
ojedat25 <toniojeda2015@gmail.com>
2024-10-28 02:54:53 -0500

    Add files via upload

---
+++ HospitalSystem/dist/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"hospitalsystem","l":"HospitalSystem"},{"p":"hospitalsystem","l":"HospitalView"}];updateSearchResults();
---
+++ HospitalSystem/src/hospitalsystem/HospitalSystemController.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hospitalsystem;
+
+/**
+ *
+ * @author edowa
+ */
+
+public class HospitalSystemController {
+        private HospitalSystemModel m;
+        private HospitalSystemPatientView pv; //This class will be for our Patient Window Display
+    private LogInView v;
+    public HospitalSystemController(HospitalSystemModel m, LogInView v){
+        this.m = m;
+        this.v = v;
+        
+    }
+    public void start(){
+        v.startup();
+    }
+    public void validateStaff(int staffId, String password){
+        boolean validation = m.checkStaff(staffId,password);
+        if(validation){
+            pv.patientdisplay(); //This function should display our patient display window
+        }
+    }
+}
+
+
